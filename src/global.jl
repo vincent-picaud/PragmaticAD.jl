eeaa7bce5dcf62642e9a8deee285f7566e504e9a
Picaud Vincent <picaud.vincent@gmail.com>
2018-07-16 17:00:16 +0200

    New tape name

--- src/global.jl
+++ src/global.jl
@@ -4,9 +4,10 @@
 
 # +AFloat, API
 const AFloat32 = AFloat{Float32}
-tape_Float32 = Tape(Float32)
-get_tape(::Type{Float32}) = tape_Float32
+tape_AFloat32 = Tape(Float32)
+get_tape(::Type{Float32}) = tape_AFloat32
+
 # +AFloat, API
 const AFloat64 = AFloat{Float64}
-tape_Float64 = Tape(Float64)
-get_tape(::Type{Float64}) = tape_Float64
+tape_AFloat64 = Tape(Float64)
+get_tape(::Type{Float64}) = tape_AFloat64
